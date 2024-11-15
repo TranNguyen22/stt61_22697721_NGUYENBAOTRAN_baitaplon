5be5d09c3b51e48933edb9d8c151bf04cee89eda
xuantruongcox <truongcool999@gmail.com>
2024-11-15 22:28:16 +0700

    FIX BUG

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ function loadCSS(filename, integrity, crossOrigin) {
   link.href = filename;
   document.head.appendChild(link);
 }
-loadCSS("assets/Fonts/font-awesome-6-pro/css/all.min.css");
-loadCSS("assets/css/main.css");
-loadCSS("assets/css/tailwind.css");
+loadCSS("https://trannguyen22.github.io/stt61_22697721_nguyenbaotran_baitaplon/assets/Fonts/font-awesome-6-pro/css/all.min.css");
+loadCSS("https://trannguyen22.github.io/stt61_22697721_nguyenbaotran_baitaplon/assets/css/main.css");
+loadCSS("https://trannguyen22.github.io/stt61_22697721_nguyenbaotran_baitaplon/assets/css/tailwind.css");
 loadCSS(
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
   "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
