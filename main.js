619b1b0b9a284beebd043edf789795edf4ebabc0
xuantruongcox <truongcool999@gmail.com>
2024-11-15 22:15:52 +0700

    FIX BUG

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ function loadCSS(filename, integrity, crossOrigin) {
   link.href = filename;
   document.head.appendChild(link);
 }
-loadCSS("/assets/Fonts/font-awesome-6-pro/css/all.min.css");
-loadCSS("/assets/css/main.css");
-loadCSS("/assets/css/tailwind.css");
+loadCSS("assets/Fonts/font-awesome-6-pro/css/all.min.css");
+loadCSS("assets/css/main.css");
+loadCSS("assets/css/tailwind.css");
 loadCSS(
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
   "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
@@ -23,7 +23,6 @@ loadCSS(
 // MAIN
 const headerElm = $("header");
 const footerElm = $("footer");
-footerElm.load("/components/footer.html");
-headerElm.load("/components/navbar.html");
-
+footerElm.load("https://trannguyen22.github.io/stt61_22697721_nguyenbaotran_baitaplon/components/footer.html");
+headerElm.load("https://trannguyen22.github.io/stt61_22697721_nguyenbaotran_baitaplon/components/navbar.html");
 
